4ee6a42153c7940a9719aa66e8edb36685921dfc
Gerald Versluis <gerald@verslu.is>
2021-05-17 10:24:03 +0200

    Implement SnackBar and Toast

--- XCTSnackbarSample/MainPage.xaml.cs
+++ XCTSnackbarSample/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.CommunityToolkit.Extensions;
+using Xamarin.CommunityToolkit.UI.Views.Options;
 
 namespace XCTSnackbarSample
 {
@@ -13,6 +15,29 @@ namespace XCTSnackbarSample
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var actions = new SnackBarActionOptions
+            {
+                Action = () => DisplayAlert("Thank you!", "Subscribed now!", "OK"),
+                Text = "No"
+            };
+
+            var options = new SnackBarOptions
+            {
+                MessageOptions = new MessageOptions
+                {
+                    Foreground = Color.White,
+                    Message = "Did you subscribe to my channel yet?!"
+                },
+                BackgroundColor = Color.FromHex("#2196F3"),
+                Duration = TimeSpan.FromSeconds(10),
+                Actions = new[] { actions }
+            };
+
+            await (sender as VisualElement).DisplaySnackBarAsync(options);
         }
     }
 }
